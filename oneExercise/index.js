104f18e665adf3fc52eff7228acc045a553867b0
Yamil <dazayamil07@gmail.com>
2022-07-08 23:02:11 -0300

    Comment

--- oneExercise/index.js
+++ oneExercise/index.js
@@ -1,2 +1,3 @@
-//Primer ejercicio del OpenBootcamp
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+//Primer ejercicio de Javascript con Node en OpenBootcamp.
+//Creamos nuestro entorno de node con un script que contenga en su valor texto definido por el mismo ejericio.
+//Creamos nuestro script saludar, y lo ejecutamos con npm run saludar.
