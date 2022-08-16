1d56164f60966b2e81276f07715c083042364e60
Yamil Daza <dazayamil07@gmail.com>
2022-08-16 20:17:40 -0300

    drag and drop

--- exercise16/app.js
+++ exercise16/app.js
@@ -1,10 +1,10 @@
 //Obtenemos todos los elementos con la clase parrafo de nuestro HTML
 const parrafo = document.querySelectorAll('.parrafo');
 
-//Obtenemos todos los elementos con la clase section de nuestro HTML
+// Obtenemos todos los elementos con la clase section de nuestro HTML
 const seccion = document.querySelectorAll('section');
 
-//Por cada parrafo le asignamos un evento, utilizando el metodo forEach
+// Por cada parrafo le asignamos un evento, utilizando el metodo forEach
 parrafo.forEach((p) => {
     p.addEventListener('dragstart', (e) => {
         const contentParrafo = e.target.textContent
@@ -20,21 +20,70 @@ parrafo.forEach((p) => {
     });
 });
 
-//Por cada seccion de nuestro HTML le vamos asignar algun evento, utilizando forEach
+// Por cada seccion de nuestro HTML le vamos asignar algun evento, utilizando forEach
 seccion.forEach((s) => {
     s.addEventListener('dragover',(e) => {
         e.preventDefault();
         console.log('DRAGOVER');
-        //Icono que nos aparece por debajo del cursos, cancelado
-        // e.dataTransfer.dropEffect = 'none';
+        // Icono que nos aparece por debajo del cursos, cancelado
+        e.dataTransfer.dropEffect = 'none';
     });
 
     s.addEventListener('drop',(e) => {
         console.log('DROP');
         console.log(e.dataTransfer);
     });
-});
+}); 
 
 
 
+
+
+//OPCION PROFESOR
+
+/* const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event => {
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".imagen-fantasma")
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
+    })
+
+    parrafo.addEventListener("dragend", () => {
+        // console.log("He terminado de arrastrar")
+        parrafo.classList.remove("dragging")
+    })
+})
+
+secciones.forEach(seccion => {
+    seccion.addEventListener("dragover", event => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy" // copy por defecto
+        // console.log("Drag Over")
+    })
+
+    seccion.addEventListener("drop", event => {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        // console.log("Párrafo id: ", id_parrafo)
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
+
+const papelera = document.querySelector(".papelera")
+
+papelera.addEventListener("dragover", event => {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "copy"
+})
+
+papelera.addEventListener("drop", event => {
+    const id_parrafo = event.dataTransfer.getData("id")
+    document.getElementById(id_parrafo).remove()
+}) */
 
